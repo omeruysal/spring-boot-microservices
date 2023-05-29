0d33baadb5e6599d4ee320c8a2f3f3c7a897c759
Omer <teouysal@outlook.com>
2023-05-30 00:00:55 +0300

    added payment service

---
+++ payment-service/src/main/java/com/example/paymentservice/controller/PaymentController.java
@@ -0,0 +1,33 @@
+package com.example.paymentservice.controller;
+
+import com.example.paymentservice.model.PaymentRequest;
+import com.example.paymentservice.model.PaymentResponse;
+import com.example.paymentservice.service.PaymentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payment")
+@AllArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest) {
+        return new ResponseEntity<>(
+                paymentService.doPayment(paymentRequest),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable String orderId) {
+        return new ResponseEntity<>(
+                paymentService.getPaymentDetailsByOrderId(orderId),
+                HttpStatus.OK
+        );
+    }
+}
---
+++ payment-service/src/main/java/com/example/paymentservice/service/PaymentService.java
@@ -0,0 +1,58 @@
+package com.example.paymentservice.service;
+import com.example.paymentservice.entity.TransactionDetails;
+import com.example.paymentservice.model.PaymentMode;
+import com.example.paymentservice.model.PaymentRequest;
+import com.example.paymentservice.model.PaymentResponse;
+import com.example.paymentservice.repository.TransactionDetailsRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+@Log4j2
+@AllArgsConstructor
+public class PaymentService {
+
+        private TransactionDetailsRepository transactionDetailsRepository;
+
+        public long doPayment(PaymentRequest paymentRequest) {
+            log.info("Recording Payment Details: {}", paymentRequest);
+
+            TransactionDetails transactionDetails
+                    = TransactionDetails.builder()
+                    .paymentDate(Instant.now())
+                    .paymentMode(paymentRequest.getPaymentMode().name())
+                    .paymentStatus("SUCCESS")
+                    .orderId(paymentRequest.getOrderId())
+                    .referenceNumber(paymentRequest.getReferenceNumber())
+                    .amount(paymentRequest.getAmount())
+                    .build();
+
+            transactionDetailsRepository.save(transactionDetails);
+
+            log.info("Transaction Completed with Id: {}", transactionDetails.getId());
+
+            return transactionDetails.getId();
+        }
+
+        public PaymentResponse getPaymentDetailsByOrderId(String orderId) {
+            log.info("Getting payment details for the Order Id: {}", orderId);
+
+            TransactionDetails transactionDetails
+                    = transactionDetailsRepository.findByOrderId(Long.valueOf(orderId));
+
+            PaymentResponse paymentResponse
+                    = PaymentResponse.builder()
+                    .paymentId(transactionDetails.getId())
+                    .paymentMode(PaymentMode.valueOf(transactionDetails.getPaymentMode()))
+                    .paymentDate(transactionDetails.getPaymentDate())
+                    .orderId(transactionDetails.getOrderId())
+                    .status(transactionDetails.getPaymentStatus())
+                    .amount(transactionDetails.getAmount())
+                    .build();
+
+            return paymentResponse;
+        }
+}
